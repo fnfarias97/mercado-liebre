02593be3794215ea15fda7eb6ba81a3692cfd16e
fnfarias97 <fnfarias97@gmail.com>
2020-09-15 21:09:36 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+// const fs = require('fs');
+const path = require('path');
+const app = express();
+
+
+
+app.listen(3030, () => console.log('Server running in 3030 port'));
+
+app.get('/', (req,res) =>{
+	let myFile = path.resolve('./vistas/index.html');
+	res.sendFile(myFile);
+});
+
+app.get('*', (req,res) =>{
+	if (req.url.includes('.')) {
+		let myFile = path.resolve('public' + req.url);
+		return res.sendFile(myFile);
+	}
+	res.send('Not Found');
+});
